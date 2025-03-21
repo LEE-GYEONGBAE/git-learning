a641fb74b04aee0cfbedc320feea87cbbc11320f
lee_gyeongbae <showtsa@gmail.com>
2025-03-21 16:16:31 +0900

    clean

---
+++ src/main/java/Barista.java
@@ -0,0 +1,31 @@
+public class Barista {
+    CoffeeMachine machine = new CoffeeMachine();
+
+    public void turnOn() {
+        machine.turnOn();
+    }
+
+    public void fillCoffee() {
+        machine.fillCoffee();
+    }
+
+    public void fillWater() {
+        machine.fillWater();
+    }
+
+    public void makeCoffee() {
+        machine.makeCoffee();
+    }
+
+    public void clearMachine() {
+        machine.clean();
+    }
+
+    public void turnOff() {
+        machine.turnOff();
+    }
+
+    public void info() {
+        machine.info();
+    }
+}
